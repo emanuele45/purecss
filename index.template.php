4fd7d5451e37bb852485326932ab0ce0a3f349d4
emanuele <emanuele45@gmail.com>
2015-04-01 12:55:02 +0200

    Changes for the general aspect (menu and stuff like that, index.template)

--- index.template.php
+++ index.template.php
@@ -38,12 +38,50 @@
  * wants and or needs.
  */
 
+class Purecss_Integrate
+{
+	public static function integrate_messageindex_topics(&$sort_column, &$indexOptions)
+	{
+		if (!isset($indexOptions['custom_selects']))
+			$indexOptions['custom_selects'] = array();
+
+		$indexOptions['custom_selects'][] = 'mf.body AS full_body';
+	}
+
+	protected static function test()
+	{
+	}
+
+	public static function integrate_messageindex_listing($topics_info)
+	{
+		global $context;
+
+		foreach ($topics_info as $row)
+		{
+			$context['topics'][$row['id_topic']]['full_body'] = parse_bbc($row['full_body']);
+		}
+	}
+	public function test_in()
+	{
+	}
+}
+
 /**
  * Initialize the template... mainly little settings.
  * load any data and set any hardcoded options.
  */
 function template_init()
 {
+	add_integration_function('integrate_messageindex_topics', 'Purecss_Integrate::integrate_messageindex_topics', '', false);
+	add_integration_function('integrate_messageindex_listing', 'Purecss_Integrate::integrate_messageindex_listing', '', false);
+
+	loadCSSFile('http://yui.yahooapis.com/pure/0.6.0/pure.css');
+	loadCSSFile('http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css');
+	loadCSSFile('side-menu.css');
+	loadCSSFile('pricing.css');
+	loadCSSFile('blog.css');
+
+	loadJavascriptFile('ui.js', array('defer' => true));
 	return array(
 		/* Use images from default theme when using templates from the default theme?
 		  if this is 'always', images from the default theme will be used.
@@ -58,7 +96,7 @@ function template_init()
 		'require_theme_strings' => false,
 
 		// This is used for the color variants.
-		'theme_variants' => array('light', 'besocial'),
+// 		'theme_variants' => array('light', 'besocial'),
 
 		// If the following variable is set to true, the avatar of the last poster will be displayed on the boardindex and message index.
 		'avatars_on_indexes' => true,
@@ -79,12 +117,12 @@ function template_init()
 		// @todo - God it's still ugly though. Can't we just have links where we need them, without all those spans?
 		// How do we get anchors only, where they will work? Spans and strong only where necessary?
 		'page_index_template' => array(
-			'base_link' => '<li class="linavPages"><a class="navPages" href="{base_link}" role="menuitem">%2$s</a></li>',
-			'previous_page' => '<span class="previous_page" role="menuitem">{prev_txt}</span>',
-			'current_page' => '<li class="linavPages"><strong class="current_page" role="menuitem">%1$s</strong></li>',
-			'next_page' => '<span class="next_page" role="menuitem">{next_txt}</span>',
-			'expand_pages' => '<li class="linavPages expand_pages" role="menuitem" {custom}> <a href="#">...</a> </li>',
-			'all' => '<span class="linavPages all_pages" role="menuitem">{all_txt}</span>',
+			'base_link' => '<a class="button-small pure-button" href="{base_link}" role="menuitem">%2$s</a>',
+			'previous_page' => '<span class="previous_page button-small pure-button" role="menuitem">{prev_txt}</span>',
+			'current_page' => '<strong class="current_page button-small pure-button" role="menuitem">%1$s</strong>',
+			'next_page' => '<span class="next_page button-small pure-button" role="menuitem">{next_txt}</span>',
+			'expand_pages' => '<span class="button-small pure-button expand_pages" role="menuitem" {custom}> <a href="#">...</a> </span>',
+			'all' => '<span class="button-small pure-button all_pages" role="menuitem">{all_txt}</span>',
 		),
 
 		// @todo find a better place if we are going to create a notifications template
@@ -223,42 +261,30 @@ function template_body_above()
 	<a href="#top" id="gotop" title="', $txt['go_up'], '">&#8593;</a>
 	<a href="#bot" id="gobottom" title="', $txt['go_down'], '">&#8595;</a>';
 
-	// Skip nav link.
+	// WAI-ARIA a11y tweaks have been applied here.
 	echo '
-	<div id="top_section">
-		<div class="wrapper">';
+	<div id="layout">
+		<!-- Menu toggle -->
+		<a href="#menu" id="menuLink" class="menu-link menu-link-toggle" data-menu="menu" data-layout="menuLink">
+			<!-- Hamburger icon -->
+			<i class="fa fa-bars fa-2x"></i>
+		</a>
+		', template_menu(), '
+		<div id="main">
+			<div class="header">
+				<div class="pure-g">';
 
 	call_template_callbacks('th', $context['theme_header_callbacks']);
 
 	echo '
-		</div>
-		<div id="header" class="wrapper', !empty($settings['header_layout']) ? ($settings['header_layout'] == 1 ? ' centerheader' : ' rightheader') : '', '"', empty($context['minmax_preferences']['upshrink']) ? '' : ' style="display: none;" aria-hidden="true"', '>
-			<h1 id="forumtitle">
-				<a href="', $scripturl, '">', $context['forum_name'], '</a>
-			</h1>';
-
-	echo '
-			<div id="logobox">
-				<img id="logo" src="', $context['header_logo_url_html_safe'], '" alt="', $context['forum_name_html_safe'], '" title="', $context['forum_name_html_safe'], '" />', empty($settings['site_slogan']) ? '' : '
-				<div id="siteslogan">' . $settings['site_slogan'] . '</div>', '
-			</div>';
-
-	// Show the menu here, according to the menu sub template.
-	echo '
-		</div>';
-
-	// WAI-ARIA a11y tweaks have been applied here.
-	echo '
-		<div id="menu_nav" role="navigation">
-			', template_menu(), '
-		</div>
-	</div>
-	<div id="wrapper" class="wrapper">
-		<div id="upper_section"', empty($context['minmax_preferences']['upshrink']) ? '' : ' style="display: none;" aria-hidden="true"', '>';
+				</div>
+			</div>
+			<div class="pure-g">';
 
 	call_template_callbacks('uc', $context['upper_content_callbacks']);
 
 	echo '
+			</div>
 		</div>';
 
 	// Show the navigation tree.
@@ -281,19 +307,25 @@ function template_th_login_bar()
 	global $context, $modSettings, $txt, $scripturl, $settings;
 
 		echo '
-			<div id="top_section_notice" class="user">
-				<form action="', $scripturl, '?action=login2;quicklogin" method="post" accept-charset="UTF-8" ', empty($context['disable_login_hashing']) ? ' onsubmit="hashLoginPassword(this, \'' . $context['session_id'] . '\');"' : '', '>
-					<div id="password_login">
-						<input type="text" name="user" size="10" class="input_text" placeholder="', $txt['username'], '" />
-						<input type="password" name="passwrd" size="10" class="input_password" placeholder="', $txt['password'], '" />
-						<select name="cookielength">
+			<div id="top_section_notice" class="pure-u-1 pure-u-md-2-5">
+				<form class="pure-form pure-g" action="', $scripturl, '?action=login2;quicklogin" method="post" accept-charset="UTF-8" ', empty($context['disable_login_hashing']) ? ' onsubmit="hashLoginPassword(this, \'' . $context['session_id'] . '\');"' : '', '>
+					<div class="pure-u-1-4">
+						<input type="text" name="user" size="10" class="pure-input-1" placeholder="', $txt['username'], '" />
+					</div>
+					<div class="pure-u-1-4">
+						<input type="password" name="passwrd" size="10" class="pure-input-1" placeholder="', $txt['password'], '" />
+					</div>
+					<div class="pure-u-1-4">
+						<select class="pure-input-1" name="cookielength">
 							<option value="60">', $txt['one_hour'], '</option>
 							<option value="1440">', $txt['one_day'], '</option>
 							<option value="10080">', $txt['one_week'], '</option>
 							<option value="43200">', $txt['one_month'], '</option>
 							<option value="-1" selected="selected">', $txt['forever'], '</option>
 						</select>
-						<input type="submit" value="', $txt['login'], '" class="button_submit" />
+					</div>
+					<div class="pure-u-1-4">
+						<button type="submit" class="pure-button pure-button-primary pure-input-1">', $txt['login'], '</button>
 					</div>
 					<input type="hidden" name="hash_passwrd" value="" />
 					<input type="hidden" name="old_hash_passwrd" value="" />
@@ -315,11 +347,13 @@ function template_th_search_bar()
 {
 	global $context, $modSettings, $txt, $scripturl;
 
+	$pure_class = !empty($modSettings['search_dropdown']) ? 'pure-u-1-3' : 'pure-u-1-2';
 	echo '
-			<form id="search_form" action="', $scripturl, '?action=search;sa=results" method="post" accept-charset="UTF-8">
-				<label for="quicksearch">
-					<input type="text" name="search" id="quicksearch" value="" class="input_text" placeholder="', $txt['search'], '" />
-				</label>';
+		<div class="pure-u-1 pure-u-md-2-5">
+			<form id="search_form" class="pure-form pure-g" action="', $scripturl, '?action=search;sa=results" method="post" accept-charset="UTF-8">
+				<div class="', $pure_class, '">
+					<input type="text" name="search" id="quicksearch" value="" class="pure-input-1" placeholder="', $txt['search'], '" />
+				</div>';
 
 	// Using the quick search dropdown?
 	if (!empty($modSettings['search_dropdown']))
@@ -327,8 +361,8 @@ function template_th_search_bar()
 		$selected = !empty($context['current_topic']) ? 'current_topic' : (!empty($context['current_board']) ? 'current_board' : 'all');
 
 		echo '
-				<label for="search_selection">
-				<select name="search_selection" id="search_selection">
+				<div class="', $pure_class, '">
+				<select class="pure-input-1" name="search_selection" id="search_selection">
 					<option value="all"', ($selected == 'all' ? ' selected="selected"' : ''), '>', $txt['search_entireforum'], ' </option>';
 
 		// Can't limit it to a specific topic if we are not in one
@@ -349,7 +383,7 @@ function template_th_search_bar()
 		echo '
 					<option value="members"', ($selected == 'members' ? ' selected="selected"' : ''), '>', $txt['search_members'], ' </option>
 				</select>
-				</label>';
+				</div>';
 	}
 
 	// Search within current topic?
@@ -362,9 +396,12 @@ function template_th_search_bar()
 				<input type="hidden" name="', (!empty($modSettings['search_dropdown']) ? 'sd_brd[' : 'brd['), $context['current_board'], ']"', ' value="', $context['current_board'], '" />';
 
 	echo '
-				<input type="submit" name="search;sa=results" value="', $txt['search'], '" class="button_submit', (!empty($modSettings['search_dropdown'])) ? ' with_select' : '', '" />
+				<div class="', $pure_class, '">
+					<button type="submit" name="search;sa=results" class="pure-button pure-button-primary pure-input-1">', $txt['search'], '</button>
+				</div>
 				<input type="hidden" name="advanced" value="0" />
-			</form>';
+			</form>
+		</div>';
 }
 
 /**
@@ -378,7 +415,7 @@ function template_uc_news_fader()
 	if (!empty($settings['enable_news']) && !empty($context['random_news_line']))
 	{
 		echo '
-			<div id="news">
+			<div id="news" class="pure-u-1">
 				<h2>', $txt['news'], '</h2>
 				', template_news_fader(), '
 			</div>';
@@ -491,50 +528,45 @@ function theme_linktree($default = 'linktree')
  */
 function template_menu()
 {
-	global $context, $txt;
+	global $context, $txt, $scripturl;
 
 	// WAI-ARIA a11y tweaks have been applied here.
 	echo '
-					<ul id="main_menu" class="wrapper" role="menubar">';
-
-	// The upshrink image, right-floated.
-	echo '
-						<li id="collapse_button" class="listlevel1">
-							<a class="linklevel1">
-								<span id="upshrink_header">&nbsp;
-									<span id="upshrink" class="collapse" style="display: none;" title="', $txt['upshrink_description'], '"></span>
-								</span>
-							</a>
-						</li>';
+				<div id="menu">
+					<div class="pure-menu">
+						<h1 class="pure-menu-heading" id="forumtitle">
+							<a href="', $scripturl, '">', $context['forum_name'], '</a>
+						</h1>
+						<ul id="main_menu" class="pure-menu-list" role="menubar">';
 
 	foreach ($context['menu_buttons'] as $act => $button)
 	{
 		echo '
-						<li id="button_', $act, '" class="listlevel1', !empty($button['sub_buttons']) ? ' subsections" aria-haspopup="true"' : '"', ' role="menuitem">
-							<a ', (!empty($button['data-icon']) ? 'data-icon="' . $button['data-icon'] . '" ' : ''), 'class="linklevel1', !empty($button['active_button']) ? ' active' : '', (!empty($button['indicator']) ? ' indicator' : '' ), '" href="', $button['href'], '" ', isset($button['target']) ? 'target="' . $button['target'] . '"' : '', '><span class="button_title">', $button['title'], '</span></a>';
+						<li id="button_', $act, '" class="pure-menu-item', !empty($button['sub_buttons']) ? ' pure-menu-item pure-menu-has-children pure-menu-allow-hover" aria-haspopup="true"' : '"', ' role="menuitem">
+							<a ', (!empty($button['data-icon']) ? 'data-icon="' . $button['data-icon'] . '" ' : ''), 'class="pure-menu-link linklevel1', !empty($button['active_button']) ? ' pure-menu-selected' : '', (!empty($button['indicator']) ? ' indicator' : '' ), '" href="', $button['href'], '" ', isset($button['target']) ? 'target="' . $button['target'] . '"' : '', '><span class="button_title">', $button['title'], '</span></a>';
 
 		// Any 2nd level menus?
 		if (!empty($button['sub_buttons']))
 		{
 			echo '
-							<ul class="menulevel2" role="menu">';
+							<ul class="pure-menu-children menulevel2" role="menu">';
 
 			foreach ($button['sub_buttons'] as $childact => $childbutton)
 			{
 				echo '
-								<li id="button_', $childact, '" class="listlevel2', !empty($childbutton['sub_buttons']) ? ' subsections" aria-haspopup="true"' : '"', ' role="menuitem">
-									<a class="linklevel2" href="', $childbutton['href'], '" ', isset($childbutton['target']) ? 'target="' . $childbutton['target'] . '"' : '', '>', $childbutton['title'], '</a>';
+								<li id="button_', $childact, '" class="pure-menu-item listlevel2', !empty($childbutton['sub_buttons']) ? ' pure-menu-has-children pure-menu-allow-hover" aria-haspopup="true"' : '"', ' role="menuitem">
+									<a class="pure-menu-link linklevel2" href="', $childbutton['href'], '" ', isset($childbutton['target']) ? 'target="' . $childbutton['target'] . '"' : '', '>', $childbutton['title'], '</a>';
 
 				// 3rd level menus :)
 				if (!empty($childbutton['sub_buttons']))
 				{
 					echo '
-									<ul class="menulevel3" role="menu">';
+									<ul class="pure-menu-children menulevel3" role="menu">';
 
 					foreach ($childbutton['sub_buttons'] as $grandchildact => $grandchildbutton)
 						echo '
-										<li id="button_', $grandchildact, '" class="listlevel3" role="menuitem">
-											<a class="linklevel3" href="', $grandchildbutton['href'], '" ', isset($grandchildbutton['target']) ? 'target="' . $grandchildbutton['target'] . '"' : '', '>', $grandchildbutton['title'], '</a>
+										<li id="button_', $grandchildact, '" class="pure-menu-item listlevel3" role="menuitem">
+											<a class="pure-menu-link linklevel3" href="', $grandchildbutton['href'], '" ', isset($grandchildbutton['target']) ? 'target="' . $grandchildbutton['target'] . '"' : '', '>', $grandchildbutton['title'], '</a>
 										</li>';
 
 					echo '
@@ -558,35 +590,8 @@ function template_menu()
 
 	// Define the upper_section toggle in javascript.
 	echo '
-				<script><!-- // --><![CDATA[
-					var oMainHeaderToggle = new elk_Toggle({
-						bToggleEnabled: true,
-						bCurrentlyCollapsed: ', empty($context['minmax_preferences']['upshrink']) ? 'false' : 'true', ',
-						aSwappableContainers: [
-							\'upper_section\',\'header\'
-						],
-						aSwapClasses: [
-							{
-								sId: \'upshrink\',
-								classExpanded: \'collapse\',
-								titleExpanded: ', JavaScriptEscape($txt['upshrink_description']), ',
-								classCollapsed: \'expand\',
-								titleCollapsed: ', JavaScriptEscape($txt['upshrink_description']), '
-							}
-						],
-						oThemeOptions: {
-							bUseThemeSettings: ', $context['user']['is_guest'] ? 'false' : 'true', ',
-							sOptionName: \'minmax_preferences\',
-							sSessionId: elk_session_id,
-							sSessionVar: elk_session_var,
-							sAdditionalVars: \';minmax_key=upshrink\'
-						},
-						oCookieOptions: {
-							bUseCookie: elk_member_id == 0 ? true : false,
-							sCookieName: \'upshrink\'
-						}
-					});
-				// ]]></script>';
+				</div>
+			</div>';
 }
 
 /**
@@ -617,7 +622,7 @@ function template_button_strip($button_strip, $direction = '', $strip_options = 
 	{
 		if (!isset($value['test']) || !empty($context[$value['test']]))
 			$buttons[] = '
-								<li role="menuitem"><a' . (isset($value['id']) ? ' id="button_strip_' . $value['id'] . '"' : '') . ' class="linklevel1 button_strip_' . $key . (!empty($value['active']) ? ' active' : '') . '" href="' . $value['url'] . '"' . (isset($value['custom']) ? ' ' . $value['custom'] : '') . '>' . $txt[$value['text']] . '</a></li>';
+								<a' . (isset($value['id']) ? ' id="button_strip_' . $value['id'] . '"' : '') . ' class="pure-button pure-button-primary button_strip_' . $key . (!empty($value['active']) ? ' active' : '') . '" href="' . $value['url'] . '"' . (isset($value['custom']) ? ' ' . $value['custom'] : '') . '>' . $txt[$value['text']] . '</a>';
 	}
 
 	// No buttons? No button strip either.
@@ -625,9 +630,9 @@ function template_button_strip($button_strip, $direction = '', $strip_options = 
 		return;
 
 	echo '
-							<ul role="menubar" class="buttonlist', !empty($direction) ? ' float' . $direction : '', '"', (empty($buttons) ? ' style="display: none;"' : ''), (!empty($strip_options['id']) ? ' id="' . $strip_options['id'] . '"' : ''), '>
+							<div role="menubar" class="pure-u-1 pure-u-md-1-2 buttonlist', !empty($direction) ? ' float' . $direction : '', '"', (empty($buttons) ? ' style="display: none;"' : ''), (!empty($strip_options['id']) ? ' id="' . $strip_options['id'] . '"' : ''), '>
 								', implode('', $buttons), '
-							</ul>';
+							</div>';
 }
 
 /**
@@ -774,22 +779,20 @@ function template_pagesection($button_strip = false, $strip_direction = '', $opt
 {
 	global $context;
 
-	// Hmmm. I'm a tad wary of having floatleft here but anyway............
-	// @todo - Try using table-cell display here. Should do auto rtl support. Less markup, less css. :)
 	if (!empty($options['page_index_markup']))
-		$pages = '<ul ' . (isset($options['page_index_id']) ? 'id="' . $options['page_index_id'] . '" ' : '') . 'class="pagelinks floatleft" role="menubar">' . $options['page_index_markup'] . '</ul>';
+		$pages = '<div ' . (isset($options['page_index_id']) ? 'id="' . $options['page_index_id'] . '" ' : '') . 'class="pagelinks pure-u-1 pure-u-md-1-2" role="menubar">' . $options['page_index_markup'] . '</div>';
 	else
 	{
 		if (!isset($options['page_index']))
 			$options['page_index'] = 'page_index';
-		$pages = empty($context[$options['page_index']]) ? '' : '<ul ' . (isset($options['page_index_id']) ? 'id="' . $options['page_index_id'] . '" ' : '') . 'class="pagelinks floatleft" role="menubar">' . $context[$options['page_index']] . '</ul>';
+		$pages = empty($context[$options['page_index']]) ? '' : '<div ' . (isset($options['page_index_id']) ? 'id="' . $options['page_index_id'] . '" ' : '') . 'class="pagelinks pure-u-1 pure-u-md-1-2" role="menubar">' . $context[$options['page_index']] . '</div>';
 	}
 
 	if (!isset($options['extra']))
 		$options['extra'] = '';
 
 	echo '
-			<div class="pagesection" role="application">
+			<div class="pure-g pricing-table" role="application">
 				', $pages, '
 				', !empty($button_strip) && !empty($context[$button_strip]) ? template_button_strip($context[$button_strip], $strip_direction) : '',
 	$options['extra'], '
